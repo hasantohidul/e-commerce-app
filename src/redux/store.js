ed22d083eeff7b3e454cb57fc8b806231760e020
Mohammad Tohidul Hasan <tohidul9849@gmail.com>
2024-09-23 22:35:31 +0600

    refactor: clean up store configuration by removing a unused import

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,4 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import cardReducer from "./cartSlice";
-// import productReducer from "./productSlice";
-
-// export const store = configureStore({
-//   reducer: {
-//     cart: cardReducer,
-//     products: productReducer,
-//   },
-// });
-
-import { configureStore, createReducer } from "@reduxjs/toolkit";
+import { configureStore} from "@reduxjs/toolkit";
 import productSlice from "./productSlice";
 import cartSlice from "./cartSlice";
 
